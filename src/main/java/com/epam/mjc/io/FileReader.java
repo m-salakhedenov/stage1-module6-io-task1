5f6cc17fe1b6d4494faa8a7afe68857777de3fbc
m-salakhedenov <m.salakhedenov@gmail.com>
2022-10-24 01:53:05 +0600

    implemented logic for creating and parsing Profile objects from files

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String rawProfileData = getRawData(file);
+
+        if (rawProfileData != null) {
+            ProfileParser parser = new ProfileParser(rawProfileData);
+            return parser.parse();
+        }
+
+        return null;
     }
+
+    private String getRawData(File file) {
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            StringBuilder fileData = new StringBuilder();
+
+            int c;
+            while ((c = reader.read()) != -1) {
+                fileData.append((char) c);
+            }
+
+            return fileData.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
