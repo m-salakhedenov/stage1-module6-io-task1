dbb4112abbf71c53e1c09e4f5169654b4dfa3c39
m-salakhedenov <m.salakhedenov@gmail.com>
2022-10-24 01:51:21 +0600

    created profile parser class

---
+++ src/main/java/com/epam/mjc/io/ProfileParser.java
@@ -0,0 +1,49 @@
+package com.epam.mjc.io;
+
+public class ProfileParser {
+
+    private String data;
+
+    public ProfileParser() {
+    }
+
+    public ProfileParser(String rawData) {
+        this.data = rawData.strip();
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Profile parse() {
+        Profile profile = new Profile();
+
+        String[] lines = data.split("\\r?\\n");
+
+        for (String line : lines) {
+            String[] keyValue = line.split("\\s*:\\s*");
+
+            switch (keyValue[0]) {
+                case "Name":
+                    profile.setName(keyValue[1]);
+                    break;
+                case "Age":
+                    profile.setAge(Integer.parseInt(keyValue[1]));
+                    break;
+                case "Email":
+                    profile.setEmail(keyValue[1]);
+                    break;
+                case "Phone":
+                    profile.setPhone(Long.parseLong(keyValue[1]));
+                    break;
+            }
+        }
+
+        return profile;
+    }
+
+}
